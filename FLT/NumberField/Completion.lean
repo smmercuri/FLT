b9aaf4a9c7670a0a1a17105acd00c62f2b3b6a6a
Salvatore Mercuri <smercuri@ed.ac.uk>
2025-01-16 08:43:38 +0000

    Remove unnecessary Algebra instance

--- FLT/NumberField/Completion.lean
+++ FLT/NumberField/Completion.lean
@@ -17,20 +17,8 @@ variable {K L : Type*} [Field K] [Field L] [Algebra K L] {v : InfinitePlace K} {
 theorem algebraMap_eq_coe :
     (algebraMap K v.Completion).toFun = ((↑) : K → v.Completion) := rfl
 
-noncomputable instance : Algebra K (w.Completion) where
-  toFun k := algebraMap L w.Completion (algebraMap K L k)
-  map_one' := by simp only [map_one]
-  map_mul' k₁ k₂ := by simp only [map_mul]
-  map_zero' := by simp only [map_zero]
-  map_add' k₁ k₂ := by simp only [map_add]
-  commutes' k lhat := mul_comm _ _
-  smul_def' k lhat := by
-    rw [RingHom.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk, UniformSpace.Completion.smul_def,
-    ← RingHom.comp_apply, ← IsScalarTower.algebraMap_eq,
-    UniformSpace.Completion.map_smul_eq_mul_coe, UniformSpace.Completion.algebraMap_def]
-
 instance {wv : v.ExtensionPlace L} : Algebra v.Completion wv.1.Completion :=
-  (mapOfComp wv.abs_comp_eq).toAlgebra
+  mapOfComp wv.abs_comp_eq |>.toAlgebra
 
 open UniformSpace.Completion in
 def extensionPlaceContinuousAlgHom (wv : v.ExtensionPlace L) :
